be5d58116493caea1008a19df14f244cd8b23966
Vic Wang <jerrywangvic@gmail.com>
2016-10-07 12:06:29 -0700

    switched offer string into offer_id in requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006173821) do
+ActiveRecord::Schema.define(version: 20161007183935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,10 +61,10 @@ ActiveRecord::Schema.define(version: 20161006173821) do
     t.string   "message"
     t.integer  "post_id"
     t.integer  "user_id"
-    t.string   "offer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "state"
+    t.integer  "offer_id"
     t.index ["post_id"], name: "index_requests_on_post_id", using: :btree
     t.index ["user_id"], name: "index_requests_on_user_id", using: :btree
   end
