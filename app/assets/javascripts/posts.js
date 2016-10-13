f4187e59480b1b32efa87edfa30261a728da8610
Vic Wang <jerrywangvic@gmail.com>
2016-10-13 16:03:21 -0700

    more polishing

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,20 +1,3 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-
-// full screen search begin
-$(function () {
-  console.log("hi vic");
-    $('a[href="#search"]').on('click', function(event) {
-        event.preventDefault();
-        $('#search').addClass('open');
-        $('#search > form > input[type="search"]').focus();
-    });
-
-    $('#search, #search button.close').on('click keyup', function(event) {
-        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-            $(this).removeClass('open');
-        }
-    });
-});
-// full screen search ends
