e6d4d4f6269edaf5a66080e7e7985dbe7ec474fc
Vic Wang <jerrywangvic@gmail.com>
2016-12-06 22:41:26 -0800

    include local FontAwesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+
+# attempting to precompile the whole `fonts` folder to use with FontAwesome local css
+Rails.application.config.assets.precompile += %w( fonts )
